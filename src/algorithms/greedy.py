Fixed numerical issues.

src/algorithms/greedy.py
@@ -3,7 +3,7 @@ from itertools import combinations
 from typing import List
 
 from src.utils.verifications import is_orthonormal_basis
-from src.utils.graphs import path, comet, sensor
+from src.utils.graph_generator import path, comet, sensor
 
 
 # Avoid KeyError by using a sorted tuple as key
@@ -48,13 +48,53 @@ def compute_greedy_basis_undirected(n: int, weights: List[List[int]]) -> np.ndar
     return np.column_stack(basis[::-1])
 
 
-def run_example(n: int, weights: List[List[int]]):
-    basis = compute_greedy_basis_undirected(n, weights)
-    np.set_printoptions(precision=3, suppress=True, linewidth=100)
-    print("Greedy basis:")
-    print(basis)
-    assert is_orthonormal_basis(basis)
+def compute_greedy_basis_directed(n: int, weights: List[List[int]]) -> np.ndarray:
+    """Computes the greedy basis.
+
+    Args:
+        n (int): The number of vertices
+        weights (List[List[int]]): The weights for a directed graph input for n vertices
+
+    Returns:
+        np.ndarray: An orthonormal basis
+    """
+    tau = {(1 << i) for i in range(n)}
+    memo = {(1 << i, 1 << j): weights[i][j] for j in range(n) for i in range(n)}
+    basis = []
+    for _ in range(n - 1):
+        A, B = max(
+            [(A, B) for A in tau for B in tau if A != B],
+            key=lambda ab: memo[*ab] / (ab[0].bit_count() * ab[1].bit_count()),
+        )
+        del memo[A, B]
+        del memo[B, A]
+        tau -= {A, B}
+        memo.update({(A | B, C): memo[A, C] + memo[B, C] for C in tau})
+        memo.update({(C, A | B): memo[C, A] + memo[C, B] for C in tau})
+        tau.add(A | B)
+        t = 1 / np.sqrt(A.bit_count() * B.bit_count() * (A.bit_count() + B.bit_count()))
+        a = np.isin(np.arange(n), [i for i in range(32) if (A >> i) & 0x1]).astype(int)
+        b = np.isin(np.arange(n), [i for i in range(32) if (B >> i) & 0x1]).astype(int)
+        u = -t * B.bit_count() * a + t * A.bit_count() * b
+        basis.append(u)
+    basis.append(np.sqrt(1 / n) * np.ones(n))
+    return np.column_stack(basis[::-1])
+
+
+def run_example(n: int, weights: List[List[int]], is_directed: bool = False):
+    if is_directed:
+        basis = compute_greedy_basis_directed(n, weights)
+        print("Greedy basis directed:")
+        print(basis)
+        assert is_orthonormal_basis(basis)
+    else:
+        basis = compute_greedy_basis_undirected(n, weights)
+        print("Greedy basis undirected:")
+        print(basis)
+        assert is_orthonormal_basis(basis)
 
 
 if __name__ == "__main__":
-    run_example(*comet(5))
+    np.set_printoptions(precision=3, suppress=True, linewidth=100)
+    run_example(*comet(6), is_directed=False)
+    run_example(*comet(6), is_directed=True)

src/algorithms/l1_norm.py
@@ -2,16 +2,16 @@ import numpy as np
 from functools import reduce, partial
 from typing import List
 
-from src.utils.measurements import S
+from src.utils.measurements import S, S_directed
 from src.utils.verifications import is_orthonormal_basis
-from src.utils.graphs import comet
+from src.utils.graph_generator import comet
 
 MAX_SIZE = 8
 
 
 def get_all_partition_matrices(n: int, m: int):
     """Generating all partition matrices for a signal of size n with m different values."""
-    assert MAX_SIZE >= n >= m >= 2
+    assert n >= m >= 2
     M = np.zeros((n, m))
 
     def f(i, free, toBeUsed):
@@ -71,12 +71,31 @@ def compute_l1_norm_basis_undirected(n: int, weights: List[List[int]]) -> np.nda
     Returns:
         np.ndarray: An orthonormal basis
     """
-    assert 2 <= n <= MAX_SIZE
-    assert len(weights) == n and all(len(row) == n for row in weights)
     u1 = np.ones(n) / np.sqrt(n)
     u2 = min(
         map(compute_first_x, get_all_partition_matrices(n, 2)),
         key=partial(S, weights=weights),
+    )
+    basis = reduce(
+        partial(expand_basis_set, weights=weights, n=n), range(3, 1 + n), [u1, u2]
+    )
+    return np.column_stack(basis)
+
+
+def compute_l1_norm_basis_directed(n: int, weights: List[List[int]]) -> np.ndarray:
+    """Computes the l1 norm basis under the function S(x) in exponential time.
+
+    Args:
+        n (int): The number of vertices
+        weights (List[List[int]]): The weights for an undirected graph input for n vertices
+
+    Returns:
+        np.ndarray: An orthonormal basis
+    """
+    u1 = np.ones(n) / np.sqrt(n)
+    u2 = min(
+        map(compute_first_x, get_all_partition_matrices(n, 2)),
+        key=partial(S_directed, weights=weights),
     )
     basis = reduce(
         partial(expand_basis_set, weights=weights, n=n), range(3, 1 + n), [u1, u2]

src/algorithms/laplacian.py
@@ -1,19 +1,22 @@
 import numpy as np
 from typing import List
+import networkx as nx
+from scipy.linalg import eigh
 
 from src.utils.verifications import is_orthonormal_basis
-from src.utils.graphs import comet
+from src.utils.graph_generator import comet
+from src.utils.visualisation import visualize_graph
 
 
 def laplacian_matrix(n: int, weights: List[List[int]]):
-    degree_matrix = np.zeros((n, n))
-    for i in range(n):
-        degree_matrix[i][i] = sum(weights[i])
-    return degree_matrix - np.array(weights)
+    g = nx.from_numpy_array(np.array(weights), create_using=nx.DiGraph)
+    return nx.laplacian_matrix(g).toarray()
 
 
 def compute_laplacian_basis(n: int, weights: List[List[int]]):
-    return np.linalg.eig(laplacian_matrix(n, weights)).eigenvectors
+    laplacian = laplacian_matrix(n, weights)
+    _, eigenvectors = eigh(laplacian)
+    return eigenvectors
 
 
 def run_example(n, weights):